e7c64bb33b16cb4f5718ce6082017c29ddfd81e1
Yinh27 <air12735@naver.com>
2023-08-11 00:33:43 +0900

    fix PortParse.py

--- PortParse.py
+++ PortParse.py
@@ -1,18 +1,10 @@
-
 def portParsing(portFormat):
     ports = []
-    if "-" in portFormat : ## ex. -p 50-80
+    if "-" in portFormat:  ## ex. -p 50-80
         start_port, end_port = map(int, portFormat.split("-"))
-        ports = range(start_port, end_port)
-    elif "," in portFormat : ## ex. 55,56,57
-        #ports = [int(x) for x in portFormat]
-        return portFormat
-    elif portFormat.isdigit() : ## ex. -p 55
-        return int(portFormat)
-    elif not portFormat.isdigt() : ## ex. -p ssh
-        pass
-    else:
-        ports = range(1,1024) ## default port setting : 1~1024
-    
+        ports = range(start_port, end_port + 1)
+    elif "," in portFormat:  ## ex. 55,56,57
+        ports = [int(x) for x in portFormat.split(",")]
+    elif portFormat.isdigit():  ## ex. -p 55
+        return [int(portFormat)]
     return ports
-
