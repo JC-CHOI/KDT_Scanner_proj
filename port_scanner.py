2f1f5be07c769b105d2a8d95064cc0847caf4c61
Yinh27 <air12735@naver.com>
2023-08-15 17:19:19 +0900

    Change log level

--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,9 @@
 import threading
 import scantype
 from scapy.all import *
+import logging
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 def get_src_port(use_rand_src):
     if use_rand_src:
@@ -17,11 +20,20 @@ def perform_port_scan(target_host, num_threads, target_ports, scan_type, use_ran
         if scan_type == "syn":
             thread = threading.Thread(target=scantype.syn_scanner, args=(target_host, port_segments[i], get_src_port(use_rand_src), results))
         elif scan_type == "tcp":
-            thread = threading.Thread(target=scantype.tcp_connect_scan, args=(target_host, port_segments[i], get_src_port(use_rand_src), results))
+            thread = threading.Thread(target=scantype.tcp_connect_scan, args=(target_host, port_segments[i], results))
         threads.append(thread)
         thread.start()
 
     for thread in threads:
         thread.join()
-
-    return results
+        
+    results_with_services = []
+    for port in results:
+        try:
+            service_name = socket.getservbyport(port)
+            results_with_services.append((port, service_name))
+        except (OSError, socket.error, socket.herror, socket.gaierror, socket.timeout):
+            results_with_services.append((port, "Unknown"))
+    
+    results_with_services.sort()
+    return results_with_services
