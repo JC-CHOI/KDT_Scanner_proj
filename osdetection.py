93bc480559c9cbcf8e5a5652b6b1b842d56d14b2
glass1222 <63572476+glass1222@users.noreply.github.com>
2023-08-17 18:13:21 +0900

    Update osdetection.py
    scapy 빼고, subprocess 활용하여 코드 수정

--- osdetection.py
+++ osdetection.py
@@ -1,11 +1,19 @@
-from scapy.all import *
+import subprocess
+import re
 
 def get_ttl(target_host):
-    pkt = IP(dst=target_host) / ICMP()
-    response = sr1(pkt, timeout=1, verbose=0)
-    if response:
-        return response.ttl
-    else:
+    try:
+        result = subprocess.run(["ping", "-n", "1", target_host], capture_output=True, text=True, timeout=5)
+        output = result.stdout
+
+        ttl_match = re.search(r"TTL=(\d+)", output) # TTL 값을 정규 표현식을 사용하여 추출
+        if ttl_match:
+            ttl = int(ttl_match.group(1))
+            return ttl
+        else:
+            return None
+    except subprocess.TimeoutExpired:
+        print("Ping timeout.")
         return None
 
 def fingerprint_os(ttl):
@@ -19,12 +27,12 @@ def fingerprint_os(ttl):
         return "Unknown"
 
 def os_detect(target_host):
-
     ttl = get_ttl(target_host)
+
     if ttl is not None:
-        print(f"TTL 값 : {target_host}: {ttl}")
         os_guess = fingerprint_os(ttl)
-        print(f"예상되는 OS: {os_guess}")
+        print(f"Target : {target_host}")
+        print(f"TTL : {ttl}")
+        print(f"OS Guess based on TTL : {os_guess}")
     else:
-        print(f"탐지에 실패 :  {target_host}")
-        
+        print("TTL not found.")
